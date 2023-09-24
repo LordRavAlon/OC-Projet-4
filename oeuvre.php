b2fc6d5537cbcafaba9ad97e22fc479be408fa6c
LordRavAlon <122441699+LordRavAlon@users.noreply.github.com>
2023-09-24 13:25:47 +0200

    Move Project to GitHub

---
+++ oeuvre.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+
+<body>
+    <header>
+        <?php include('header.php'); ?>
+    </header>
+    <main>
+
+
+        <?php include('oeuvres.php'); ?>
+        <?php foreach ($oeuvres as $oeuvre): ?>
+            <?php if ($_GET['id'] == $oeuvre['id']) { ?>
+                <article id="detail-oeuvre">
+                    <div id="img-oeuvre">
+                        <img src=<?php echo $oeuvre['image src']; ?> alt=<?php echo $oeuvre['image alt']; ?>>
+                    </div>
+                    <div id="contenu-oeuvre">
+                        <h1>
+                            <?php echo $oeuvre['titre']; ?>
+                        </h1>
+                        <p class="description">
+                            <?php echo $oeuvre['description']; ?>
+                        </p>
+                        <p class="description-complete">
+                            <?php echo $oeuvre['description complete']; ?>
+                        </p>
+                    </div>
+
+                </article>
+            <?php } ?>
+        <?php endforeach ?>
+
+
+    </main>
+    <footer>
+        <?php include('footer.php'); ?>
+    </footer>
+</body>
+
+</html>
